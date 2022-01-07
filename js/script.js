64f62dda95d0aa155eb3b82dae1b78f0dbe61a31
katherine_fier <katherinefier@Katherines-MacBook-Pro.local>
2022-01-07 15:27:37 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+// Display our symbols as placeholders for the chosen word's letters
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = letterInput.value;
+  console.log(guess);
+  letterInput.value = "";
+});
