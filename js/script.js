584e9dce32f661158cbd3d54b64b950db54165ae
katherine_fier <katherinefier@Katherines-MacBook-Pro.local>
2022-06-30 09:25:09 -0600

    added functions to show guessed letters, update the word in progress, and check if the player won.

--- js/script.js
+++ js/script.js
@@ -80,5 +80,65 @@ const makeGuess = function (guess){
   else {
     guessedLetters.push (guess);
     console.log (guessedLetters);
+    showGuessedLetters();
+    updateWordInProgress (guessedLetters);
+  }
+};
+
+const showGuessedLetters = function () {
+
+//Clear the list first
+
+guessedLettersElement.innerHTML = "";
+
+for (const letter of guessedLetters) {
+
+  const li = document.createElement ("li");
+
+  li.innerText = letter;
+
+  guessedLettersElement.append (li);
+
+}
+
+};
+
+
+const updateWordInProgress = function (guessedLetters){
+
+  const wordUpper = word.toUpperCase ();
+
+  const wordArray = wordUpper.split ("");
+
+  const revealWord = [];
+
+  for (const letter of wordArray){
+
+      if (guessedLetters.includes (letter)) {
+
+        revealWord.push(letter.toUpperCase());
+      }
+
+      else {
+
+       revealWord.push ("●")
+      }
+  }
+
+  //console.log (revealWord);
+
+  wordInProgress.innerText = revealWord.join ("");
+  
+  checkIfWin ();
+
+};
+
+const checkIfWin = function () {
+
+  if (word.toUpperCase() === wordInProgress.innerText) {
+
+    message.classList.add ("win");
+    message.innerHTML = '<p class ="highlight">You guessed the correct word! Congrats!</p>';
+
   }
 };
