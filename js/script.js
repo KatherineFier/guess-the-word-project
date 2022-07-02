1376bcb813ddcb4c0b1280479f16ccd4cf08fcfc
katherine_fier <katherinefier@Katherines-MacBook-Pro.local>
2022-07-01 18:01:18 -0600

    created function to count remaining guesses and an async function to fetch word fromAPI txt file

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+
+const getWord = async function () {
+  const response = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+  const words = await response.text();
+  const wordArray = words.split ("\n");
+  const randomIndex = Math.floor (Math.random () * wordArray.length);
+  word = wordArray[randomIndex].trim();
+  placeholder (word);
+};
+
+getWord();
 
 // Display our symbols as placeholders for the chosen word's letters
 const placeholder = function (word) {
@@ -130,6 +143,35 @@ const updateWordInProgress = function (guessedLetters){
   wordInProgress.innerText = revealWord.join ("");
   
   checkIfWin ();
+
+};
+
+
+const updateGuessesRemaining = function (guess) {
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(guess)){
+
+        message.innerText = 'Sorry, the word has no ${guess}';
+        remainingGuesses -=1;
+    }
+
+    else {
+        message.innerText ='Good guess! The word has the letter ${guess}';
+    }
+
+    if (remainingGuesses === 0){
+
+        message.innerHTML = 'Game over! The word was <span class="highlight">${word}</span>;'
+    }
+
+    else if (remainingGuesses === 1){
+        remainingGuessesSpan.innerText = '${remainingGuesses} guess';
+    }
+
+    else {
+      remainingGuessesSpan.innerText = '${remainingGuesses} guess';
+    }
+
 
 };
 
